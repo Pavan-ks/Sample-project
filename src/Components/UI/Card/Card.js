295a4590095debcb5953418b4530f91383f33322
Pavan K S (BLR GSS) <pavanks@nousinfo.com>
2023-07-13 21:02:56 +0530

    added file

---
+++ src/Components/UI/Card/Card.js
@@ -0,0 +1,10 @@
+//vendors
+import React from "react";
+//css
+import "./Card.css";
+
+const Card = (props) => {
+  return <div className={`card ${props.className}`}>{props.children}</div>;
+};
+
+export default Card;
